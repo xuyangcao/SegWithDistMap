17256efddef36db32fc4c53c7d1ed2bc6b602155
xuyangcao <12213062@bjtu.edu.cn>
2019-12-25 10:02:29 +0800

    add abus training and testing list

---
+++ data/abus_data/split_data.py
@@ -0,0 +1,40 @@
+import os
+import random
+
+if __name__ == "__main__":
+    """ 
+        random split data into training set and testing set, 
+        the data was split according to the patient_id to avoid
+        volumes from same patient in both training set and testing set 
+    """
+
+    random.seed(2109)
+
+    filenames = os.listdir('./image/')
+    filenames.sort()
+    #print(len(filenames))
+
+    patient_id = [filename[:4] for filename in filenames]
+    patient_id = set(patient_id)
+    patient_id = list(patient_id)
+    patient_id.sort()
+    random.shuffle(patient_id)
+    #print(patient_id)
+    #print(len(patient_id))
+
+    train_id = patient_id[:60]
+    test_id = patient_id[60:]
+    print('train_patients: ', len(train_id))
+    print('test_patients: ', len(test_id))
+    print('train_patients: ', train_id)
+    print('test_patients: ', test_id)
+
+    with open('abus_train.list', 'w') as f:
+        for filename in filenames:
+            if filename[:4] in train_id:
+                f.write(filename+'\n')
+
+    with open('abus_test.list', 'w') as f:
+        for filename in filenames:
+            if filename[:4] in test_id:
+                f.write(filename+'\n')
